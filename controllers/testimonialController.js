c459ea4f70242db462f6c5bb7d7fa24d1144eb17
master <joelpalacio630@gmail.com>
2024-03-11 21:02:38 -0600

    feat : Validación del formulario de Testimoniales

--- controllers/testimonialController.js
+++ controllers/testimonialController.js
@@ -1,7 +1,31 @@
 
 
 const guardarTestimonial = ( req, res ) => {
-    console.log( req.body );
+
+    //  Validar el formulario
+    const { nombre, correo, mensaje } = req.body;
+    const errores = [];
+
+    if ( nombre.trim() === '' ) {
+        errores.push( { mensaje: 'El nombre está vacío' } );
+    }
+
+    if ( correo.trim() === '' ) {
+        errores.push( { mensaje: 'El correo está vacío' } );
+    }
+
+    if ( mensaje.trim() === '' ) {
+        errores.push( { mensaje: 'El mensaje está vacío' } );
+    }
+
+    if ( errores.length >= 0 ) {
+        // Mostrar la vista con errores
+        res.render( 'testimoniales', {
+            pagina: 'Testimoniales',
+            errores,
+            nombre, correo, mensaje
+        } );
+    }
 }
 
 export {
