278dfc74bde8cc5c2a94e353cdff9ff797588aa5
JPalacio3 <joelpalacio630@gmail.com>
2025-05-28 17:40:12 -0600

    feat: mejorar configuración de base de datos y actualizar descripción del proyecto

--- config/db.js
+++ config/db.js
@@ -1,21 +1,22 @@
 import { Sequelize } from "sequelize";
 
 // Importamos la funcionalidad para las variables de entorno
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 dotenv.config();
 
 // Configuración de Sequelize para conectar la base de datos de mysql :
-const db = new Sequelize( process.env.DB_URL || '127.0.0.1', {
-    define: {
-        timestamps: false
-    },
-    pool: {
-        max: 5,
-        min: 0,
-        acquire: 3000,
-        idle: 10000
-    },
-    operatorsAliases: false
-} );
+const db = new Sequelize(process.env.DB_URL, {
+  define: {
+    timestamps: false,
+  },
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 3000,
+    idle: 10000,
+  },
+  dialect: "mysql", // Aseguramos que el dialecto sea MySQL
+  operatorsAliases: false,
+});
 
 export default db;
--- index.js
+++ index.js
@@ -1,38 +1,42 @@
-import express from 'express';
-import router from './routes/index.js';
-import db from './config/db.js';
+import express from "express";
+import router from "./routes/index.js";
+import db from "./config/db.js";
 
 const app = express();
 
+db.sync() // Esto crea las tablas si no existen, según los modelos definidos
+  .then(() => console.log("Tablas creadas"))
+  .catch((error) => console.log(error));
+
 // Conectar la base de datos
 db.authenticate()
-    .then( () => console.log( 'Base de datos conectada' ) )
-    .catch( error => console.log( error ) );
+  .then(() => console.log("Base de datos conectada"))
+  .catch((error) => console.log(error));
 
 // Definir puerto
 const port = process.env.PORT || 5500;
 
 // Habilitar pug
-app.set( 'view engine', 'pug' );
+app.set("view engine", "pug");
 
 // Obtener el año actual
-app.use( ( req, res, next ) => {
-    const year = new Date();
-    res.locals.actualYear = year.getFullYear();
-    res.locals.nombreSitio = 'Agencia de Viajes';
-    return next();
-} );
+app.use((req, res, next) => {
+  const year = new Date();
+  res.locals.actualYear = year.getFullYear();
+  res.locals.nombreSitio = "Agencia de Viajes";
+  return next();
+});
 
 // Agregar body parser para leer los datos del formulario
-app.use( express.urlencoded( { extended: true } ) );
+app.use(express.urlencoded({ extended: true }));
 
 // Agregar router
 // .use() es un verbo que va a permitir soporte para todos los demás verbos establecidos en el router
-app.use( '/', router );
+app.use("/", router);
 
 // Definir la carpeta pública
-app.use( express.static( 'public' ) );
+app.use(express.static("public"));
 
-app.listen( port, () => {
-    console.log( `El servidor está corriendo en el puerto ${port}` );
-} );
+app.listen(port, () => {
+  console.log(`El servidor está corriendo en el puerto ${port}`);
+});
